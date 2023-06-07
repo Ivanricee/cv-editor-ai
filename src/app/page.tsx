5525d062cc9338f253538055892c906b80f51033
Iván Bautista <ivanbautistag@gmail.com>
2023-06-06 21:12:50 -0600

    refactor: change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,18 @@ export default async function Home() {
   const prod = '/infojobs'
 
   return (
-    <div className="card flex-shrink-0 shadow-2xl bg-base-100 w-[500px] sm:w-screen md:w-3/5 lg:w-3/6 p-10 ">
+    <div className="card flex-shrink-0 shadow-2xl bg-base-200 w-[500px] sm:w-screen md:w-3/5 lg:w-3/6 p-10 ">
       <div className="card-body hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">CV Editor AI</h1>
+          <h1 className="text-5xl font-bold">CV Enhancer AI</h1>
           <div className="py-6">
             <p className="break-words text-left">
               Simplifica la creación y edición de tu currículum vitae con
               inteligencia artificial.
             </p>
             <p className="break-words text-left py-3">
-              Sincroniza tus CV con Infojobs y guarda los cambios realizados.
+              Sincroniza tus CV con Infojobs o linkedIn y guarda los cambios
+              realizados.
             </p>
           </div>
           <div className="flex gap-8 justify-center">
