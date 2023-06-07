48d9890b4a1e3952e6edadcf5f752c87b3c19563
Iván Bautista <ivanbautistag@gmail.com>
2023-06-06 20:46:49 -0600

    fix: add only cv to cvlist. New function to set remainin cv data on close modal

--- src/stores/createCvSlice.ts
+++ src/stores/createCvSlice.ts
@@ -3,14 +3,29 @@ import { StateCreator } from 'zustand'
 
 export const createCvSlice: StateCreator<cvSlice> = (set, get) => ({
   CVList: [],
-  currentCV: null,
-
+  currentCVCode: null,
   setCVList: (CVList) =>
-    set((state) => ({
-      CVList: [...state.CVList, ...CVList],
-    })),
-  setCurrentCV: (currentCV) =>
     set({
-      currentCV,
+      CVList: [...CVList],
+    }),
+  setRemainingCvData: (code, remainCvData) =>
+    set((prevCvList) => {
+      const indexToBeEdited = prevCvList.CVList.findIndex(
+        (cvItem) => cvItem.cv?.code === code
+      )
+      let updatedCVList = [...prevCvList.CVList]
+      const cvEdited = {
+        cv: prevCvList.CVList[indexToBeEdited].cv,
+        ...remainCvData,
+      }
+      updatedCVList[indexToBeEdited] = cvEdited
+
+      return {
+        CVList: [...updatedCVList],
+      }
+    }),
+  setCurrentCvCode: (currentCVCode) =>
+    set({
+      currentCVCode,
     }),
 })
