aadf06ee7505e2419be0a8d2bf2521d4749cf1c3
Iván Bautista <ivanbautistag@gmail.com>
2023-06-12 13:50:47 -0600

    Feat: need data  from infojobs

---
+++ src/app/api/infojobs/getDictionary/route.ts
@@ -0,0 +1,23 @@
+/**
+ Personal data country:
+    https://api.infojobs.net/api/1/dictionary/country
+
+Education upper badge:
+    https://api.infojobs.net/api/1/dictionary/study
+
+education title:
+    https://api.infojobs.net/api/1/dictionary/study-detail
+
+country:
+    https://api.infojobs.net/api/1/dictionary/country
+
+Situación laboral y preferencias
+    https://developer.infojobs.net/test-console/execute.xhtml
+
+espectativas salariales
+    https://api.infojobs.net/api/1/dictionary/salary-period
+
+province
+
+    https://api.infojobs.net/api/1/dictionary/province
+ */
