25ade59f7af19e8a64fe8b73ea959ccee8de8a8c
Iván Bautista <ivanbautistag@gmail.com>
2023-06-19 21:53:33 -0600

    refactor: early return when code is null

--- src/app/api/infojobs/getToken/route.ts
+++ src/app/api/infojobs/getToken/route.ts
@@ -13,45 +13,41 @@ export async function GET(request: NextRequest) {
     return await refreshToken(cookieRToken?.value!)
   }
 
-  if (code) {
-    try {
-      let responseToken = await getNewToken('code')
-
-      if (responseToken.error && cookieRToken) {
-        responseToken = await getNewToken('refresh')
-      }
-      const { access_token, refresh_token, error, error_description } =
-        await responseToken
-
-      const response = NextResponse.json({
-        success: access_token ? true : false,
-        error,
-        error_description,
-      })
-
-      if (!error) {
-        response.cookies.set({
-          name: 'access_token',
-          value: access_token,
-          httpOnly: true,
-        })
-        response.cookies.set({
-          name: 'refresh_token',
-          value: refresh_token,
-          httpOnly: true,
-        })
-      }
-
-      return response
-    } catch (error) {
-      console.log('error: ', error)
-      return NextResponse.json({ error: 'error on request' }, { status: 500 })
-    }
-  }
   //Error
-  const response = NextResponse.json(
-    { error: 'No code provided' },
-    { status: 500 }
-  )
-  return response
+  if (code == null)
+    return NextResponse.json({ error: 'No code provided' }, { status: 400 })
+
+  try {
+    let responseToken = await getNewToken('code')
+
+    if (responseToken.error && cookieRToken) {
+      responseToken = await getNewToken('refresh')
+    }
+    const { access_token, refresh_token, error, error_description } =
+      await responseToken
+
+    const response = NextResponse.json({
+      success: access_token ? true : false,
+      error,
+      error_description,
+    })
+
+    if (!error) {
+      response.cookies.set({
+        name: 'access_token',
+        value: access_token,
+        httpOnly: true,
+      })
+      response.cookies.set({
+        name: 'refresh_token',
+        value: refresh_token,
+        httpOnly: true,
+      })
+    }
+
+    return response
+  } catch (error) {
+    console.log('error: ', error)
+    return NextResponse.json({ error: 'error on request' }, { status: 500 })
+  }
 }
